e4b157aeb1a4f876eced668c1a30dd325c6a182a
pvhee <peter.vanhee@gmail.com>
2018-01-02 15:23:51 +0100

    trial running Keras as part of a function, downloading mnist dataset

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
 # Digit Recognizer Serverless
 > Serverless endpoints to invoke a deep neural network classifier for digit recognition
+
+
+## Installation
+
+Install `serverless` globally, then you need `virtualenv` to push this to AWS.
+
+    virtualenv digit-recognizer-serverless
+    source digit-recognizer-serverless/bin/activate
+
+And then deploy the new function
+
+    serverless deploy
+
+
--- handler.py
+++ handler.py
@@ -1,10 +1,26 @@
 import json
 import random
+import keras
+from keras.models import Sequential
+from keras import layers
+from keras.datasets import mnist
+from keras import backend as K
 
 def classify(event, context):
+    (x_train, y_train), (x_test, y_test) = mnist.load_data()
+
+    x_train = x_train.reshape(60000, 784)
+    x_test = x_test.reshape(10000, 784)
+    x_train = x_train.astype('float32')
+    x_test = x_test.astype('float32')
+    x_train /= 255
+    x_test /= 255
+
     body = {
         "message": "Go Serverless v1.0! Your function executed successfully!",
         "class": random.randrange(0,100),
+        "x_train": x_train.shape[0],
+        "x_test": x_test.shape[0],
     }
 
     response = {
